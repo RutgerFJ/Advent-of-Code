961a04fa0d893cf8fa1f9548e5021c34750b012a
RutgerFJ <119589357+RutgerFJ@users.noreply.github.com>
2022-12-05 14:03:58 +0100

    Allow any length input / any crate number
    Took out some hardcoding

--- 2022/Day 5/Puzzle 1.py
+++ 2022/Day 5/Puzzle 1.py
@@ -1,15 +1,15 @@
 def main():
     with open('input.txt', 'r') as f:
-        content = [line.strip('\n\n') for line in f]
+        data = [line.strip('\n') for line in f]
 
-    stacks = [[line[4 * x + 1] for line in content if len(line) > 33 and
-               line[4 * x + 1].isalpha()] for x in range(9)]
-    moves = [line.split(' ') for line in content if len(line) in range(15, 25)]
-    
+    stacks = [[ln[4 * x + 1] for ln in data if len(ln) == len(data[0])
+               and ln[4 * x + 1].isalpha()] for x in range(len(data[0][1::4]))]
+    moves = [ln.split(' ') for ln in data if ln.split(' ')[0] == 'move']
+
     for m in moves:
         to_move = stacks[int(m[3]) - 1][0:int(m[1])]
         stacks[int(m[3]) - 1] = stacks[int(m[3]) - 1][int(m[1])::]
-        stacks[int(m[5]) - 1] = to_move[::-1] + stacks[int(m[5]) - 1]
+        stacks[int(m[5]) - 1] = to_move + stacks[int(m[5]) - 1]
 
     top_layer = ''
     for s in stacks:
@@ -18,4 +18,3 @@ def main():
 
 
 main()
-  
