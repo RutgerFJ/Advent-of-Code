ca83ea5c925bf301a6e2f2c65349d6815a55ac2c
RutgerFJ <119589357+RutgerFJ@users.noreply.github.com>
2022-12-05 14:18:34 +0100

    Deleted redundant line

--- 2022/Day 5/Puzzle 1.py
+++ 2022/Day 5/Puzzle 1.py
@@ -2,17 +2,16 @@ def main():
     with open('input.txt', 'r') as f:
         data = [line.strip('\n') for line in f]
 
-    stacks = [[ln[4 * x + 1] for ln in data if len(ln) == len(data[0])
-               and ln[4 * x + 1].isalpha()] for x in range(len(data[0][1::4]))]
+    stax = [[ln[4 * x + 1] for ln in data if len(ln) == len(data[0])
+             and ln[4 * x + 1].isalpha()] for x in range(len(data[0][1::4]))]
     moves = [ln.split(' ') for ln in data if ln.split(' ')[0] == 'move']
 
     for m in moves:
-        to_move = stacks[int(m[3]) - 1][0:int(m[1])]
-        stacks[int(m[3]) - 1] = stacks[int(m[3]) - 1][int(m[1])::]
-        stacks[int(m[5]) - 1] = to_move + stacks[int(m[5]) - 1]
+        stax[int(m[5])-1] = stax[int(m[3])-1][0:int(m[1])] + stax[int(m[5])-1]
+        stax[int(m[3])-1] = stax[int(m[3])-1][int(m[1])::]
 
     top_layer = ''
-    for s in stacks:
+    for s in stax:
         top_layer += s[0]
     print(top_layer)
 
