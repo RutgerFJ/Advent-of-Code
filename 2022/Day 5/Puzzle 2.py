4af947fc2b9e9aceefd64d59cf19d278e7878eb2
RutgerFJ <119589357+RutgerFJ@users.noreply.github.com>
2022-12-05 10:41:38 +0100

    Create Puzzle 2.py

---
+++ 2022/Day 5/Puzzle 2.py
@@ -0,0 +1,19 @@
+def main():
+    with open('input.txt', 'r') as f:
+        content = [line.strip('\n\n') for line in f]
+
+    stacks = [[line[4 * x + 1] for line in content if len(line) > 33 and
+               line[4 * x + 1].isalpha()] for x in range(9)]
+    moves = [line.split(' ') for line in content if len(line) in range(15, 25)]
+    for m in moves:
+        to_move = stacks[int(m[3]) - 1][0:int(m[1])]
+        stacks[int(m[3]) - 1] = stacks[int(m[3]) - 1][int(m[1])::]
+        stacks[int(m[5]) - 1] = to_move + stacks[int(m[5]) - 1]
+
+    top_layer = ''
+    for s in stacks:
+        top_layer += s[0]
+    print(top_layer)
+
+
+main()
