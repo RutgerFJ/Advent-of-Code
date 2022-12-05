4171b9f29bacfe1f493d270bd436a99cb2a3a961
RutgerFJ <119589357+RutgerFJ@users.noreply.github.com>
2022-12-05 10:43:11 +0100

    Update Puzzle 1.py

--- 2022/Day 5/Puzzle 1.py
+++ 2022/Day 5/Puzzle 1.py
@@ -5,6 +5,7 @@ def main():
     stacks = [[line[4 * x + 1] for line in content if len(line) > 33 and
                line[4 * x + 1].isalpha()] for x in range(9)]
     moves = [line.split(' ') for line in content if len(line) in range(15, 25)]
+    
     for m in moves:
         to_move = stacks[int(m[3]) - 1][0:int(m[1])]
         stacks[int(m[3]) - 1] = stacks[int(m[3]) - 1][int(m[1])::]
