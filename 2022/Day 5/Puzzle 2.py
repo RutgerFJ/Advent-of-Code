7637005b9cec687ed70b2a32ea7bf01038f2f04b
RutgerFJ <119589357+RutgerFJ@users.noreply.github.com>
2022-12-05 14:30:54 +0100

    Make unreadable but short

--- 2022/Day 5/Puzzle 2.py
+++ 2022/Day 5/Puzzle 2.py
@@ -1,19 +1,16 @@
 def main():
     with open('input.txt', 'r') as f:
-        content = [line.strip('\n\n') for line in f]
+        data = [line.strip('\n') for line in f]
 
-    stacks = [[line[4 * x + 1] for line in content if len(line) > 33 and
-               line[4 * x + 1].isalpha()] for x in range(9)]
-    moves = [line.split(' ') for line in content if len(line) in range(15, 25)]
+    s = [[ln[4 * x + 1] for ln in data if len(ln) == len(data[0])
+          and ln[4 * x + 1].isalpha()] for x in range(len(data[0][1::4]))]
+    moves = [ln.split(' ') for ln in data if ln.split(' ')[0] == 'move']
+
     for m in moves:
-        to_move = stacks[int(m[3]) - 1][0:int(m[1])]
-        stacks[int(m[3]) - 1] = stacks[int(m[3]) - 1][int(m[1])::]
-        stacks[int(m[5]) - 1] = to_move + stacks[int(m[5]) - 1]
+        s[int(m[5])-1] = s[int(m[3])-1][0:int(m[1])][::-1] + s[int(m[5])-1]
+        s[int(m[3])-1] = s[int(m[3])-1][int(m[1])::]
 
-    top_layer = ''
-    for s in stacks:
-        top_layer += s[0]
-    print(top_layer)
+    print(''.join([i[0] for i in s]))
 
 
 main()
