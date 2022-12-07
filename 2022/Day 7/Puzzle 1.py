0d8bdf0e06364c340b6f745f855e9ef14f4bf03b
RutgerFJ <119589357+RutgerFJ@users.noreply.github.com>
2022-12-07 19:37:43 +0100

    Create Puzzle 1.py

---
+++ 2022/Day 7/Puzzle 1.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+
+def main():
+    last_dirs, keywords, directories = [], ['$', 'cd', 'dir'], defaultdict(int)
+    with open('input.txt', 'r') as f:
+        for x in f:
+            if x.split()[-1] == '..':
+                last_dirs = last_dirs[:-1]
+            elif len(x.split()) > 2:
+                last_dirs.append(x.split()[2])
+            elif x.split()[0] not in keywords:
+                for y in range(len(last_dirs)):
+                    directories[tuple(last_dirs[:y + 1])] += int(x.split()[0])
+
+    print(sum(bits for bits in directories.values() if bits <= 100000))
+
+
+main()
