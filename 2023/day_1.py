8c3d5c15726339181ace7caaa0cd25eb218d47c8
Rutger Jansen <119589357+RutgerFJ@users.noreply.github.com>
2023-12-01 20:49:29 +0100

    Update day_1.py

--- 2023/day_1.py
+++ 2023/day_1.py
@@ -11,15 +11,15 @@ def part_one(inp):
 
 def part_two(inp):
     digit_dict = {
-        "one": '1',
-        "two": '2',
-        "three": '3',
-        "four": '4',
-        "five": '5',
-        "six": '6',
-        "seven": '7',
-        "eight": '8',
-        "nine": '9'
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9'
     }
 
     file_digits = []
