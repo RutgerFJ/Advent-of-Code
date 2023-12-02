c5f60e6e150d15ccfd2f8d7065dbf82fc117036c
Rutger Jansen <119589357+RutgerFJ@users.noreply.github.com>
2023-12-02 22:30:35 +0100

    Add files via upload

---
+++ 2023/day_2/day_2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+part_one() {
+    sum=0
+
+    while read -r line; do
+        game_num=$(echo "$line" | awk '{print substr($2,1,(length($2)-1))}')
+	over_thresh=$(echo "$line"  | egrep -o '([1-9]{1}[3-9]{1,} red|[1-9]{1}[4-9]{1,} green|[1-9]{1}[5-9]{1,} blue)')
+	if [[ ${#over_thresh} -lt 1 ]]; then
+            sum=$(($sum + $game_num))
+	fi
+    done < "input.txt";
+
+    echo "$sum"
+}
+
+part_two() {
+    sum=0
+
+    while read -r line; do
+        line_value=$(echo "$line" | sed -E 's/Game [0-9]{1,}://' | tr -d ';,' | awk '{
+            for (i=1; i<=NF; i += 2) {
+                number=$i;
+                color=$(i+1);
+                if (number > max[color]) {
+                    max[color]=number;
+                }
+            }
+        }
+        END {
+            print max["blue"] * max["red"] * max["green"];
+            }'
+        )
+        sum=$(($sum + line_value))
+    done < "input.txt";
+
+    echo "$sum"
+}
+
+main() {
+    echo "$(part_one)"
+    echo "$(part_two)"
+}
+
+main
