535ecc248e0981a6750c4fba9b378dfaaef26d45
Rutger Jansen <119589357+RutgerFJ@users.noreply.github.com>
2023-12-04 11:24:09 +0100

    Update day_4.py

--- 2023/day_4/day_4.py
+++ 2023/day_4/day_4.py
@@ -1,12 +1,9 @@
-from collections import defaultdict
-
-
 def part_one(inp):
-    card_sums = defaultdict(lambda: 0.5)
+    card_sums = {x: 0.5 for x in range(len(inp))}
 
     for x, game in enumerate(inp):
-        win_nums = {int(num) for num in game[10:40].split()}
-        hand_nums = {int(num) for num in game[42:].split()}
+        win_nums = {num for num in game[10:40].split()}
+        hand_nums = {num for num in game[42:].split()}
 
         for _ in range(len(win_nums & hand_nums)):
             card_sums[x] *= 2
@@ -15,11 +12,11 @@ def part_one(inp):
 
 
 def part_two(inp):
-    won_cards = defaultdict(lambda: 1)
+    won_cards = {x: 1 for x in range(len(inp))}
 
     for x, game in enumerate(inp):
-        win_nums = {int(num) for num in game[10:40].split()}
-        hand_nums = {int(num) for num in game[42:].split()}
+        win_nums = {num for num in game[10:40].split()}
+        hand_nums = {num for num in game[42:].split()}
 
         for y in range(x + 1, x + len(hand_nums & win_nums) + 1):
             won_cards[y] += won_cards[x]
@@ -28,7 +25,7 @@ def part_two(inp):
 
 
 def main():
-    with open('input.txt', 'r') as f:
+    with open('./AoC2023/Day 4/input.txt', 'r') as f:
         content = [line.strip() for line in f]
     
     print(part_one(content))
