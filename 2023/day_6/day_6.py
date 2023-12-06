fb9b96cde9393dbce80c1a170f1c4c4d708ff848
Rutger Jansen <119589357+RutgerFJ@users.noreply.github.com>
2023-12-06 22:33:06 +0100

    Update day_6.py
    Unpack tuples in a more readable manner

--- 2023/day_6/day_6.py
+++ 2023/day_6/day_6.py
@@ -2,9 +2,7 @@ def part_one(inp):
     races = zip(map(int, inp[0]), map(int, inp[1]))
 
     ways = 1
-    for r in races:
-        time = r[0]
-        dist = r[1]
+    for time, dist in races:
         ways *= sum([1 for ms in range(1, time) if (ms * (time - ms)) > dist])
     
     return ways
@@ -18,7 +16,7 @@ def part_two(inp):
 
 
 def main():
-    with open('input.txt', 'r') as f:
+    with open('./AoC2023/Day 6/input.txt', 'r') as f:
         content = [line.split()[1:] for line in f]
 
     print(part_one(content))
