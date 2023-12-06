768d6ddf56096b87543273d99eb3f363b4d0a55f
Rutger Jansen <119589357+RutgerFJ@users.noreply.github.com>
2023-12-06 22:58:33 +0100

    Create day_6.py

---
+++ 2023/day_6/day_6.py
@@ -0,0 +1,26 @@
+def part_one(inp):
+    races = zip(map(int, inp[0]), map(int, inp[1]))
+
+    ways = 1
+    for time, dist in races:
+        ways *= sum([1 for t in range(1, time) if t * (time - t) > dist])
+    
+    return ways
+
+
+def part_two(inp):
+    time = int(''.join(inp[0]))
+    dist = int(''.join(inp[1]))
+
+    return sum([1 for t in range(1, time) if t * (time - t) > dist])
+
+
+def main():
+    with open('input.txt', 'r') as f:
+        content = [line.split()[1:] for line in f]
+
+    print(part_one(content))
+    print(part_two(content))
+
+
+main()
