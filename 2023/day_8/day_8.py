01d9e09d043313e5c7173c0d0391160a9ffe22fa
Rutger Jansen <119589357+RutgerFJ@users.noreply.github.com>
2023-12-09 12:35:02 +0100

    Create day_8.py

---
+++ 2023/day_8/day_8.py
@@ -0,0 +1,43 @@
+from math import lcm
+import re
+
+
+def part_one(inp):
+    moves = inp[0]
+    nodes = {pos: (left, right) for pos, left, right in 
+             [re.findall('[A-Z]{3}', line) for line in inp[2:]]}
+
+    node, count = 'AAA', 0
+    while node != 'ZZZ':
+        node = nodes[node][moves[count % len(moves)] == 'R']
+        count += 1
+    
+    return count
+
+
+def part_two(inp):
+    moves = inp[0]
+    nodes = {pos: (left, right) for pos, left, right in 
+             [re.findall('[A-Z]{3}', line) for line in inp[2:]]}
+
+    a_nodes = [node for node in nodes if node[-1] == 'A']
+    cycles = []
+    for node in a_nodes:
+        count = 0
+        while not node[-1] == 'Z':
+            node = nodes[node][moves[count % len(moves)] == 'R']
+            count += 1
+        cycles.append(count)
+
+    return lcm(*cycles)
+    
+
+def main():
+    with open('input.txt', 'r') as f:
+        content = [line.strip() for line in f]
+
+    print(part_one(content))
+    print(part_two(content))
+
+
+main()
