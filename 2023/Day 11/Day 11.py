2afbf3c6d8d05d3a14e645853ecd708757d5a5ea
Rutger Jansen <rutgerfjansen@gmail.com>
2023-12-11 22:12:23 +0100

    fin

--- 2023/Day 11/Day 11.py
+++ 2023/Day 11/Day 11.py
@@ -4,11 +4,11 @@ from itertools import combinations
 def manhattan_parser(x1, y1, x2, y2):
     x = [(x, y1) for x in range(x1, x2, 1 if x2 > x1 else -1)]
     y = [(x2, y) for y in range(y1, y2, 1 if y2 > y1 else -1)]
-    
+
     return x + y, abs(x1 - x2) + abs(y1 - y2)
 
 
-def part_one_or_two(inp, exp):
+def part_one_or_two(inp, expansion):
     galaxies = [(y, x) for y, row in enumerate(inp) 
                 for x, col in enumerate(row) if col == '#']
     
@@ -17,10 +17,10 @@ def part_one_or_two(inp, exp):
     
     exp_x = [col for col in range(len(inp)) 
              if all(inp[row][col] == '.' for row in range(len(inp)))]
-    
-    return sum(dist + sum([exp for y, x in path if y in exp_y or x in exp_x])
-                           for a, b in combinations(galaxies, 2)
-                           for path, dist in [manhattan_parser(*a, *b)])
+
+    return sum(manh_dist + sum([expansion for y, x in path if y in exp_y or x in exp_x])
+               for a, b in combinations(galaxies, 2)
+               for path, manh_dist in [manhattan_parser(*a, *b)])
 
 
 if __name__ == '__main__':
